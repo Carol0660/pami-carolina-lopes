b1f82cd7ae802e04bc2da571ff658181cc0f5228
Carolina Lopes <carolcandela006@gmail>
2025-08-19 21:35:29 -0300

    App editor de fotos

--- LojadeFotos/app/(tabs)/index.tsx
+++ LojadeFotos/app/(tabs)/index.tsx
@@ -1,9 +1,23 @@
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+import { Image } from 'expo-image';
 import { Link } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
+       <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+       <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <Text style={styles.text}>Home screen</Text>
         <Link href="/about" style={styles.button}>
         Go to About screen
@@ -19,6 +33,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
   text: {
     color: '#b9c8e4ff',
   },
@@ -26,5 +48,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff',
+  },
+   footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
